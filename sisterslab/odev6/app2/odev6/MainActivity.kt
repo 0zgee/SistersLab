e4f4d7e11a3943031a6dbd2e625e16aa2fd3946d
ozgealma <89697397+ozgealma@users.noreply.github.com>
2023-08-25 21:48:28 +0300

    Add files via upload

---
+++ sisterslab/odev6/app2/odev6/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.odev6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.odev6.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNav,navHostFragment.navController)
+    }
+}
